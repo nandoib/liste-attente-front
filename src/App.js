4cdd61b091467a2b486df534f065abedab72efc4
Nando IBBA <nandoibba@gmail.com>
2023-02-03 23:25:11 +0100

    k

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 function App() {
-  return (
-    <>
-      <h1 className="text-9xl antialiased font-bold underline mx-auto">
-        Hello world!
-      </h1>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
