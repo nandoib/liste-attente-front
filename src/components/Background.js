bbb04168524bb5109a079055fb43334344c9b8f4
Nando IBBA <nandoibba@gmail.com>
2023-06-06 10:18:26 +0200

    again

--- src/components/Background.js
+++ src/components/Background.js
@@ -5,7 +5,7 @@ const Background = (props) => {
     <>
       {" "}
       <div
-        className="md:flex md:flex-wrap  md:items-center md:justify-center  md:h-screen md:py-0 my-10"
+        className="md:flex md:flex-wrap  md:items-center md:justify-center  md:h-screen md:py-0"
         style={{ backgroundImage: `url(${bgImage})` }}
       >
         <div className="w-full bg-white shadow-lg p-6 lg:w-8/12 border lg:rounded-lg lg:p-14">
