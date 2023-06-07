9a32867abe1415b1b2b9a9fb361cbe1eb0f2f4dc
Nando IBBA <nandoibba@gmail.com>
2023-06-07 18:15:28 +0200

    q

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -256,7 +256,7 @@ const AdminPage = () => {
 
         <p className=" text-center text-2xl font-bold m-2">Liste d'attente</p>
 
-        <div class="flex flex-col">
+        <div class="">
           <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
               <div class="overflow-hidden p-10">
